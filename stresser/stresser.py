8ac13523691efd4f6ae50d488b79c68d0f135b55
Aspw-w <0kcabtuo0@gmail.com>
2024-01-11 01:58:21 +0900

    s

--- stresser/stresser.py
+++ stresser/stresser.py
@@ -5,12 +5,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from time import sleep
+from os import path
 
 inputUsername = 'OmgAspwMoment'
 inputPassword = 'awawa1919!'
 
 urls = ['https://stresser.zone/register']
-s = Service(r'C:\Users\0kcab\Documents\Python\automicrosoft\chromedriver.exe')
+s = Service(path.join(path.dirname(__file__), 'chromedriver.exe'))
 
 for url in urls:
     driver = webdriver.Chrome(service=s)
@@ -20,9 +21,9 @@ driver.get(url)
 try:
     setUser = driver.find_element(By.ID, '#ここ取得できなくて困ってる')
     setUser.send_keys(inputUsername)
-    print('ぱすおっけー')
+    print('名前おっけー')
 except:
-    print('パスダメ～')
+    print('名前ダメ～')
 
 try:
     setPass = driver.find_element(By.ID, 'password')
