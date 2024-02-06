c14218c6f09ade84b1dee04bd911d86e4b7f19f4
Aspw-w <0kcabtuo0@gmail.com>
2024-02-07 01:54:24 +0900

    dwa

---
+++ nitro/main.py
@@ -0,0 +1,36 @@
+#discord.com用のプロモーションNitroを作成します
+
+import requests
+import time
+import sys
+import os
+import threading
+from concurrent.futures import ThreadPoolExecutor
+
+generateCount = int(input("Nitro作成数: "))
+
+def sendRequest():
+    url = "https://api.discord.gx.games/v1/direct-fulfillment"
+    headers = {
+        "origin": "https://www.opera.com",
+        "Content-Type": "application/json",
+        "Sec-Ch-Ua": '"Opera GX";v="105", "Chromium";v="119", "Not?A_Brand";v="24"',
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0"
+    }
+    data = {
+        "partnerUserId": "bc385c68-be5f-43c2-9713-cb2051fef65b"
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code == 200:
+        with open("generated.txt", "a") as file:
+            file.write(f"https://discord.com/billing/partner-promotions/1180231712274387115/{response.json()['token']}\n")
+        time.sleep(0.05)
+
+print(f'生成中です... (x{generateCount})')
+
+thread = ThreadPoolExecutor(max_workers=3)
+
+for i in range(generateCount):
+    thread.submit(sendRequest)
